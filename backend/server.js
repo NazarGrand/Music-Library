8bd0199f64188f0c15e24b6eced1d72fc9fd0351
NazarGrand <hryhoriak.nazarii@chnu.edu.ua>
2024-05-03 18:29:44 +0300

    feat: added cors and cookie-parser

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 require("./src/config/dbConnection");
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("hello!");
