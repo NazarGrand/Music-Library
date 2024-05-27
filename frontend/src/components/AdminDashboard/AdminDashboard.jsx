8f6be87ca79974576d1970bd29fb8713b09c6be3
NazarGrand <hryhoriak.nazarii@chnu.edu.ua>
2024-05-27 20:38:34 +0300

    feat: configure firebase and to do it in frontend

--- frontend/src/components/AdminDashboard/AdminDashboard.jsx
+++ frontend/src/components/AdminDashboard/AdminDashboard.jsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
+import { firebase } from "../../config/firebaseConfig";
+
 const AdminDashboard = () => {
-  return <p>Admin dashboard</p>;
+  const [url, setUrl] = useState("");
+
+  const handleFileUpload = (e) => {
+    const selectedFile = e.target.files[0];
+
+    if (selectedFile) {
+      const storageRef = firebase.storage().ref();
+      const fileRef = storageRef.child(selectedFile.name);
+
+      fileRef.put(selectedFile).then((snapshot) => {
+        snapshot.ref.getDownloadURL().then((downloadURL) => {
+          console.log(downloadURL);
+          setUrl(downloadURL);
+        });
+      });
+    } else {
+      console.log("No file selected!");
+    }
+  };
+
+  return (
+    <div>
+      <p>Admin dashboard</p>
+
+      <input type="file" onChange={handleFileUpload} />
+
+      <input
+        type="text"
+        placeholder="Add URL"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
+    </div>
+  );
 };
 
 export default AdminDashboard;
