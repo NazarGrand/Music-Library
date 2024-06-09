00093ed5a1efb91658f09d720ff782ce12a80e1d
NazarGrand <hryhoriak.nazarii@chnu.edu.ua>
2024-06-09 18:05:10 +0300

    fix: removed timestamps from user and verification-model

--- backend/src/models/user-model.js
+++ backend/src/models/user-model.js
@@ -1,14 +1,11 @@
 const { Schema, model } = require("mongoose");
 
-const UserSchema = new Schema(
-  {
-    userName: { type: String, required: true },
-    email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
-    status: { type: String, required: false },
-    role: { type: String, required: true },
-  },
-  { timestamps: true }
-);
+const UserSchema = new Schema({
+  userName: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  status: { type: String, required: false },
+  role: { type: String, required: true },
+});
 
 module.exports = model("User", UserSchema);
--- backend/src/models/verification-model.js
+++ backend/src/models/verification-model.js
@@ -1,12 +1,9 @@
 const { Schema, model } = require("mongoose");
 
-const VerificationSchema = new Schema(
-  {
-    user: { type: Schema.Types.ObjectId, ref: "User" },
-    isTokenUsed: Boolean,
-    verificationToken: String,
-  },
-  { timestamps: true }
-);
+const VerificationSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  isTokenUsed: Boolean,
+  verificationToken: String,
+});
 
 module.exports = model("verificationUser", VerificationSchema);
